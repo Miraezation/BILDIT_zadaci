99c7dddc771fecc786d371589643703d1a7ece84
Miralem <Miralem@Miralem-PC>
2017-11-13 23:43:13 +0100

    metode

---
+++ src/ZADACA_13_11_2017/zadatak2.java
@@ -0,0 +1,9 @@
+package ZADACA_13_11_2017;
+
+public class zadatak2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/ZADACA_13_11_2017/zadatak5.java
@@ -0,0 +1,9 @@
+package ZADACA_13_11_2017;
+
+public class zadatak5 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
