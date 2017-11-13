4ebb0cbbf1fbb100feb675fc5f03396941355393
Mirae <33443682+Miraezation@users.noreply.github.com>
2017-11-13 20:13:34 +0100

    Update zadatak2.java

--- src/ZADACA_9_11_2017/zadatak2.java
+++ src/ZADACA_9_11_2017/zadatak2.java
@@ -8,7 +8,7 @@ public class zadatak2 {
 	int	palindrom=0;
 	int i=0;
 	int z=0;
-	
+	int e=0;
 	
 	for (i=100;i<1000;i++){
 	    int zadnja=i%10;
@@ -32,17 +32,12 @@ public class zadatak2 {
         }
 
         if (palindrom==z && k==0){
-    		
-    		System.out.println(" Najveći prosti palindrom od 1 do 1000 je : " + z);}
-        
-        
+    		e=z;
+    		}
 	
-        	
-        		
-        		
-        			
-			}       
-		}
+	}       
+}
+		System.out.println(" Najveći prosti palindrom od 1 do 1000 je : " + z);
 	}
 }
 
